c3e0bd4715b277e9c08e5bfa8dce8f605c643d78
Val Shapoval <valshapoval@Vals-MacBook-Pro.local>
2022-11-16 14:49:39 +0000

    test: add e2e tests

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -18,3 +18,39 @@ test('homepage has title and links to intro page', async ({ page }) => {
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
 });
+
+test('testMain', async ({ page }) => {
+
+  await page.goto('http://mtujodo.herokuapp.com/');
+
+  await page.getByRole('heading', { name: 'Welcome to MTU Jōdō' }).click();
+
+});
+
+test('testAddCompetitor', async ({ page }) => {
+
+  await page.goto('http://mtujodo.herokuapp.com/');
+
+  await page.getByRole('button', { name: 'Competition' }).click();
+
+  await page.getByRole('link', { name: 'Add Competitor' }).click();
+  await expect(page).toHaveURL('http://mtujodo.herokuapp.com/competition/addCompetitor');
+
+});
+
+test('testViewCompetitor', async ({ page }) => {
+
+  await page.goto('http://mtujodo.herokuapp.com/');
+
+  await page.getByRole('button', { name: 'Competition' }).click();
+
+  await page.getByRole('link', { name: 'View Competitor' }).click();
+  await expect(page).toHaveURL('http://mtujodo.herokuapp.com/competition/viewCompetitor');
+
+});
+
+test('testLogin', async ({ page }) => {
+  await page.goto('http://mtujodo.herokuapp.com/');
+  await page.getByRole('link', { name: 'Login' }).click();
+  await expect(page).toHaveURL('http://mtujodo.herokuapp.com/login');
+});
