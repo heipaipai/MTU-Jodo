d1b141539aefd26dd1a7d73f496e94330e2e08ae
Val Shapoval <valshapoval@Vals-MacBook-Pro.local>
2022-11-17 09:01:08 +0000

    feat: add login

---
+++ pages/api/users.ts
@@ -0,0 +1,15 @@
+
+import dbConnect from '../../lib/mongoose'
+import Users from '../../models/users'
+
+export default async function handler(req: any, res: any) {
+  await dbConnect()
+  
+    try {
+      const user = await Users.create(req.body)
+      res.status(201).json({ success: true, data: user })
+    }
+    catch (error) {
+      res.status(400).json({ success: false })
+    } 
+}
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,9 +1,30 @@
+"use client";
 import React from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Head from 'next/head'
 
 function Login() {
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+
+  const onSubmit = async () => {
+    fetch('https://mtujodo.herokuapp.com/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    }).then((res) => {
+      if (res.status === 201) alert("success");
+
+      setEmail('');
+      setPassword('');
+    }).catch((err) => {
+      console.log(err)
+    })
+}
+
     return (
       <>
       <Head>
@@ -15,7 +36,7 @@ function Login() {
                 <div className="col-md-12 mt-5">
                     <div className="jumbotron">
                         <h1 className="display-4 text-center">Login</h1>
-                        <p className="lead text-center">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                        <p className="lead text-center">Welcome back, Official! Please, use your credentials to login below.</p>
                     </div>
                 </div>
             </div>
@@ -23,17 +44,17 @@ function Login() {
                 <div className="col-md-12 mt-5">
                     <Form>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Control type="email" placeholder="Email Address" />
+                            <Form.Control type="text" placeholder="Email Address" value={email} onSubmit={(e: any) => setEmail(e.target.value)}/>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control type="text" placeholder="Password" value={password} onSubmit={(e: any) => setPassword(e.target.value)} />
                         </Form.Group>
 
                         {}
 
                         <div className="d-grid gap-2">
-                            <Button variant="dark" type="submit">Login</Button>
+                            <Button variant="dark" type="submit" onClick={onSubmit}>Login</Button>
                         </div>
                     </Form>
                 </div>
